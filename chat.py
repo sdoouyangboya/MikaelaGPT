83e9c48ca9c9d492e5cd17e024309c97e254b284
Boya Ouyang <42460189+sdoouyangboya@users.noreply.github.com>
2023-07-20 02:12:56 -0700

    Add files via upload

--- chat.py
+++ chat.py
@@ -117,7 +117,7 @@ examples:
 # and the suffix our user input and output indicator
 
 suffix = """
-Use the above examples ,the chat history (delimited by <hs></hs>) and following context (delimited by <ctx></ctx>) to answer the question , make recommendations with corresponding Link and price of recommended experiences, pls recomend experience with larger ranking accodding to ranking column, but don't mention ranking in your response
+Use the above examples ,the chat history (delimited by <hs></hs>) and following context (delimited by <ctx></ctx>) to answer the question , make recommendations with corresponding Links, pls recomend experience with larger ranking accodding to ranking column, but don't mention ranking in your response
 ------
 <ctx>
 {context}
