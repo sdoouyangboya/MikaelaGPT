1f0122194824c3227742aed817add074ce81a557
Boya Ouyang <42460189+sdoouyangboya@users.noreply.github.com>
2023-07-20 10:41:19 -0700

    Update chat.py

--- chat.py
+++ chat.py
@@ -22,7 +22,7 @@ import pickle
 #     openai_api_key = file.read()
 openai_api_key = st.secrets["openai_api_key"]
 
-# @st.cache_resource
+@st.cache_resource
 def load_data():
     loader = CSVLoader("experiences.csv", encoding="utf-8", csv_args={
                 'delimiter': ','})
@@ -202,7 +202,7 @@ chain_type_kwargs={
 #     # Ask the question
 #     answer = qa.run({"query": question})
 #    st.write("Answer:", answer)
-# @st.cache_resource
+@st.cache_resource
 def get_answer(message):
     return qa.run({"query": message["content"]})
 
